d312a091949cfc97b529c1648246a65240e0b17d
yashg <yash.gupta2361@gmail.com>
2023-05-12 16:01:22 +0530

    commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,13 +3,9 @@ import "./Sidebar.css";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import SidebarOption from "./SidebarOption";
 import HomeIcon from "@material-ui/icons/Home";
-import SearchIcon from "@material-ui/icons/Search";
-import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
-import MailOutlineIcon from "@material-ui/icons/MailOutline";
-import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
-import ListAltIcon from "@material-ui/icons/ListAlt";
 import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+// import LogoutIcon from '@mui/icons-material/Logout';
 import { Button } from "@material-ui/core";
 
 function Sidebar() {
@@ -18,13 +14,8 @@ function Sidebar() {
       <TwitterIcon className="sidebar__twitterIcon" />
 
       <SidebarOption active Icon={HomeIcon} text="Home" />
-      <SidebarOption Icon={SearchIcon} text="Explore" />
-      <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
-      <SidebarOption Icon={MailOutlineIcon} text="Messages" />
-      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
-      <SidebarOption Icon={ListAltIcon} text="Lists" />
       <SidebarOption Icon={PermIdentityIcon} text="Profile" />
-      <SidebarOption Icon={MoreHorizIcon} text="More" />
+      <SidebarOption Icon={MoreHorizIcon} text="Logout" />
 
       {/* Button -> Tweet */}
       <Button variant="outlined" className="sidebar__tweet" fullWidth>
--- src/Widgets.js
+++ src/Widgets.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Widgets.css";
+import "./Feed.css"
 import {
   TwitterTimelineEmbed,
   TwitterShareButton,
@@ -9,29 +10,30 @@ import SearchIcon from "@material-ui/icons/Search";
 
 function Widgets() {
   return (
-    <div className="widgets">
-      <div className="widgets__input">
-        <SearchIcon className="widgets__searchIcon" />
-        <input placeholder="Search Twitter" type="text" />
-      </div>
+    // <div className="widgets">
+    //   <div className="widgets__input">
+    //     <SearchIcon className="widgets__searchIcon" />
+    //     <input placeholder="Search Twitter" type="text" />
+    //   </div>
 
-      <div className="widgets__widgetContainer">
-        <h2>What's happening</h2>
+    //   <div className="widgets__widgetContainer">
+    //     <h2>What's happening</h2>
 
-        <TwitterTweetEmbed tweetId={"858551177860055040"} />
+    //     <TwitterTweetEmbed tweetId={"858551177860055040"} />
 
-        <TwitterTimelineEmbed
-          sourceType="profile"
-          screenName="cleverqazi"
-          options={{ height: 400 }}
-        />
+    //     <TwitterTimelineEmbed
+    //       sourceType="profile"
+    //       screenName="cleverqazi"
+    //       options={{ height: 400 }}
+    //     />
 
-        <TwitterShareButton
-          url={"https://facebook.com/cleverprogrammer"}
-          options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
-        />
-      </div>
-    </div>
+    //     <TwitterShareButton
+    //       url={"https://facebook.com/cleverprogrammer"}
+    //       options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
+    //     />
+    //   </div>
+    // </div>
+    <div className="whead">Following</div>
   );
 }
 
